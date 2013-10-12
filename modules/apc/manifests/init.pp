ed6b26ef2178050b7b91e5ac320e4f5981ca6a5c
Georgiana Gligor <georgiana.gligor@gmail.com>
2013-10-12 13:57:19 +0300

    #1 fix apc `vagrant provision` error * renamed the execution tasks to comply with the standard used in the php module

--- modules/apc/manifests/init.pp
+++ modules/apc/manifests/init.pp
@@ -3,19 +3,20 @@ class apc {
     require apache
     require php
 
-    exec { 'apc install':
+    exec { 'apc::install':
         command => 'pecl install apc',
+        unless  => 'php -i | grep "apc.enabled"',
     }
     
-    exec { 'apc php extension enable':
+    exec { 'apc::php-extension-enable':
         command => 'echo "extension=apc.so" > /etc/php.d/apc.ini'
     }
     
-    exec { 'apc copy config file':
+    exec { 'apc::copy-config-file':
         command => 'cp /usr/share/pear/apc.php /var/www/html/',
     }
     
-    exec { 'restart apache':
+    exec { 'apc::restart-apache':
         command => 'service httpd restart',
     }
 
