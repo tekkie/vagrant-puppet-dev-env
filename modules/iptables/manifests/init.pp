4c51a48ea24b2d126bc321e0390a8d07f18c44cd
AndreiMuresan <andrei@cloudtroopers.ro>
2013-10-16 08:59:28 +0300

    Fixed Iptables issue - added service restart command

--- modules/iptables/manifests/init.pp
+++ modules/iptables/manifests/init.pp
@@ -3,6 +3,7 @@ class iptables {
     file { '/etc/sysconfig/iptables' :
         source => "puppet:///modules/iptables/iptables"
     }
-
-    
+   
+    exec { iptables_restart: command => "/etc/init.d/iptables restart"
+    }
 }
